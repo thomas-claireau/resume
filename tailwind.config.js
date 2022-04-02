cf38516bf9a8edf91075df9134c22194c8168abb
Thomas Claireau <thomas.claireau@gmail.com>
2022-04-02 08:40:20 +0200

    :sparkles: add gray color default

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
-		fontFamily: {
-			'sans': ['Nunito', 'sans-serif'],
-		}
+    fontFamily: {
+      sans: ['Nunito', 'sans-serif'],
+    },
+    colors: {
+      gray: '#545454',
+    },
   },
   plugins: [],
-}
+};
