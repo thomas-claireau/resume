0e73c88fc2149c7bf6737c493eb3aa552d54615d
Thomas Claireau <thomas.claireau@gmail.com>
2022-04-05 11:23:27 +0200

    :globe_with_meridians: (content): add content

--- locales/en.js
+++ locales/en.js
@@ -3,5 +3,102 @@ export default {
   contact: 'Get In Touch',
   job: 'Fullstack Web Developer',
   h1: 'Hello, I\'m Thomas 👋',
-  description: 'I\'m a 26 years old fullstack web developer with 4 years of experience in JavaScript and PHP.',
+  description: 'I\'m 26 and I\'m a fullstack web developer with 4 years of experience in JavaScript and PHP.',
+  buttons: {
+    portfolio: 'See my portfolio',
+    blog: 'Blog',
+    linktree: 'Linktree',
+  },
+  projects: {
+    label: 'Projects',
+    items: [
+      {
+        title: 'Frontend React developer',
+        description: 'Support of the development team + management of the front tickets of the client project.',
+        organization: 'La Compagnie Hyperactive',
+        date: 'March 2022 - April 2022',
+      },
+      {
+        title: 'Mentor Web development',
+        description: 'I\'m helping students during formation (frontend or backend)',
+        organization: 'Openclassrooms',
+        date: 'November 2020 - Today',
+      },
+      {
+        title: 'Backend PHP Symfony developer',
+        description: 'Development of a web application in PHP and vanilla javascript interface',
+        organization: 'Royal Punk',
+        date: 'September 2021 - February 2022',
+      },
+      {
+        title: 'Web interface designer',
+        description: 'Design of a web interface for a bakery',
+        organization: 'Feeling Good Bakery',
+        link: 'https://feelinggoodbakery.fr/',
+        date: 'March 2021 - June 2021',
+      },
+    ],
+  },
+  formation: {
+    label: 'Education',
+    items: [
+      {
+        title: 'Frontend Web Developer',
+        organization: 'Openclassrooms',
+        date: '2020',
+      },
+      {
+        title: 'Backend PHP / Symfony developer',
+        organization: 'Openclassrooms',
+        date: '2019',
+      },
+      {
+        title: 'Master 2 STAPS',
+        organization: 'University Rennes II',
+        date: '2018',
+      },
+    ],
+  },
+  skills: {
+    label: 'Skills',
+    items: [
+      {
+        title: 'React / Next.js',
+      },
+      {
+        title: 'PHP / Symfony',
+      },
+      {
+        title: 'NodeJS / Express',
+      },
+      {
+        title: 'Vanilla JS / Sass',
+      },
+    ],
+  },
+  tools: {
+    label: 'Tools',
+    items: [
+      {
+        title: 'Visual Studio Code',
+      },
+      {
+        title: 'Gitflow',
+      },
+      {
+        title: 'Figma / Adobe XD / Photoshop',
+      },
+    ],
+  },
+  interests: {
+    label: 'Interests',
+    items: [
+      {
+        title: 'Travel ✈️',
+      },
+      {
+        title: 'Tennis 🎾',
+      },
+    ],
+  },
 };
